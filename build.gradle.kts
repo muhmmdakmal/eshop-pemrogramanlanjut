11df59bfc7def13ea3959eb830a5c6faeee9d455
muhmmdakmal <muhmmdakmal.69@gmail.com>
2025-02-18 22:12:51 +0700

    Fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ tasks.withType<Test>().configureEach {
 
 tasks.test{
     filter{
-        excludeTestsMatching("FunctionalTest")
+        excludeTestsMatching("*FunctionalTest")
     }
 
     finalizedBy(tasks.jacocoTestReport)
